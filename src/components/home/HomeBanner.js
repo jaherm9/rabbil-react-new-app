4b58ae33784d2e74f2b1dfff822c283446d97cf8
shaka <jaherm9@gmail.com>
2022-09-11 23:48:09 +0600

    axios added

--- src/components/home/HomeBanner.js
+++ src/components/home/HomeBanner.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
-const HomeBanner = (props) => {
+const HomeBanner = () => {
+
+    let [todo, SetTodo] = useState([])
+useEffect(()=>{             // useEffect are use for load component data Automatically
+
+    // API Call
+    // axios.get().then().catch()
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+        .then((res)=>{
+            setTimeout(()=>{
+                SetTodo(res.data)
+            }, 3000)
+        })
+        .catch((err)=>{
+
+        })
+})
     return (
         <div>
-            <h1>{props.title}</h1>
-            <h1>{props.subtitle}</h1>
-
+            {JSON.stringify(todo)}
         </div>
     );
 };
