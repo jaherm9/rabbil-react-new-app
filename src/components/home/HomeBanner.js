0fb0c68f8970242ae10639f5617d8f59f31ed104
shaka <jaherm9@gmail.com>
2022-09-12 09:34:38 +0600

    13 Listing completed

--- src/components/home/HomeBanner.js
+++ src/components/home/HomeBanner.js
@@ -13,15 +13,33 @@ const HomeBanner = () => {
         })
     },[])      //[] penalty array used for stop auto recalling
 
+    const DeleteEvent=(id)=>{
+        let DeleteID = "Delete ID ="+ id
+        alert(DeleteID)
+        //API CALL Delete
+
+    }
+    const EditEvent=(id)=>{
+        let EditID = "Edit ID ="+ id
+        alert(EditID)
+        //API CALL Edit
+    }
+    const DetailsEvent=(id)=>{
+        let DetailsID = "Details ID ="+ id
+        alert(DetailsID)
+        //API CALL Details
+    }
+
 const MyList = todo.map((list,i)=>{
     return(
         <tr>
             <td>{list['userId']}</td>
             <td>{list['id']}</td>
             <td>{list['title']}</td>
-            <td><button className="btn btn-danger">Delete</button></td>
-            <td><button className="btn btn-primary">Edit</button></td>
-            <td><button className="btn btn-success">Details</button></td>
+            <td><button onClick={DeleteEvent.bind(this,list['id'])} className="btn btn-danger">Delete</button></td>
+            // use bind and this for prevent random selection
+            <td><button onClick={EditEvent.bind(this,list['id'])} className="btn btn-primary">Edit</button></td>
+            <td><button onClick={DetailsEvent.bind(this,list['id'])} className="btn btn-success">Details</button></td>
         </tr>
     )
 })
